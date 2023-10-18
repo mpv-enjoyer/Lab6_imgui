e241a8fae7e38f0d21414d3a3bc335c97097cd38
DilsThik <Disblocker.just@gmail.com>
2023-10-18 23:45:34 +0300

    huh

--- main.h
+++ main.h
@@ -40,3 +40,17 @@ static void glfw_error_callback(int error, const char* description)
 #include <ctime>
 
 #define INTSTR(int) std::to_string(int)
+std::vector<int> method(int N, std::vector<std::vector<int>>& distance, int step);
+
+// Функции для вычисления точного оптимального значения
+void print_optimal(std::vector<std::vector<int>>& distance);
+void optimal_internal(std::vector<bool> used, std::vector<int> trace, int overall_distance, std::vector<std::vector<int>>& distance);
+bool is_optimal(int input, bool RESET = false);
+std::vector<int> get_current_optimal_trace(std::vector<int> trace_input = {});
+// ...
+
+// Разное
+std::string to_string(std::vector<int> input);
+int randomnumber(int included_min, int included_max);
+void print_step(int step, std::vector<int> trace, int selected_town, int overall_distance, std::vector<std::vector<int>>& distance);
+// ...
--- method.cpp
+++ method.cpp
@@ -1,28 +1,7 @@
 #include "main.h"
 
-// Входные данные
-const int N = 5; 
-std::vector<std::vector<int>> distance =
-   {{ 0, 5, 8, 5, 3 },                     
-    { 5, 0, 2, 4, 1 },
-    { 8, 2, 0, 6, 7 },
-    { 5, 4, 6, 0, 9 },
-    { 3, 1, 7, 9, 0 }}; // Оптимальное решение для этой задачи коммивояжёра: 5 -> 1 -> 2 -> 4 -> 3 : расстояние 8
-// ...
-
-// Функции для вычисления точного оптимального значения
-void print_optimal();
-void optimal_internal(std::vector<bool> used, std::vector<int> trace, int overall_distance);
-bool is_optimal(int input);
-// ...
-
-// Разное
-std::string to_string(std::vector<int> input);
-int randomnumber(int included_min, int included_max);
-void print_step(int step, std::vector<int> trace, int selected_town, int overall_distance);
-// ...
-
-int method()
+std::vector<std::vector<int>> optimal_stack_INTERNAL;
+std::vector<int> method(int N, std::vector<std::vector<int>>& distance, int step)
 {
     std::vector<bool> X = std::vector<bool>(N, false); // Если мы уже входили в город, пишем 1
     std::vector<int> trace;
@@ -32,7 +11,7 @@ int method()
     trace.push_back(x);
     std::cout << "1) Trace " << to_string(trace) << " Select " << x << "\n";
 
-    for (int i = 1; i < N; i++) //общий шаг
+    for (int i = 1; (i < step + 1) && (i < N); i++) //общий шаг
     {
         int current_town = x;
         std::vector<int> current_min_pairs;
@@ -65,39 +44,49 @@ int method()
         trace.insert(trace.begin() + current_min_from + 1, current_min_to);
         X[current_min_to] = true;
         overall_distance += current_min_distance_in_pairs;
-        print_step(i + 1, trace, x, overall_distance);
+        print_step(i + 1, trace, x, overall_distance, distance);
     }
     overall_distance += distance[trace[0]][trace[N-1]];
 
     std::cout << "Final: Trace " << to_string(trace) << " Distance " << overall_distance << "\n";
 
     std::cout << "Calculating best possible result:\n";
-    print_optimal();
+    print_optimal(distance);
 }
 
 // Код функций для вычисления точного оптимального значения
-void print_optimal()
+void print_optimal(std::vector<std::vector<int>>& distance)
 {
-    optimal_internal({ 1, 0, 0, 0, 0}, { 0 }, 0);
-    optimal_internal({ 0, 1, 0, 0, 0}, { 1 }, 0);
-    optimal_internal({ 0, 0, 1, 0, 0}, { 2 }, 0);
-    optimal_internal({ 0, 0, 0, 1, 0}, { 3 }, 0);
-    optimal_internal({ 0, 0, 0, 0, 1}, { 4 }, 0);
+    for (int i = 0; i < distance.size(); i++)
+    {
+        std::vector<bool> used;
+        for (int j = 0; j < distance.size(); j++)
+        {
+            if (i == j) used.push_back(1);
+            else used.push_back(0);
+        }
+        optimal_internal(used, { i }, 0, distance);
+    }
 }
-bool is_optimal(int input)
+bool is_optimal(int input, bool RESET)
 {
     static int current_optimal = __INT_MAX__;
-    if (input <= current_optimal) 
+    if (RESET) 
+    {
+        current_optimal = __INT_MAX__;
+        return 0;
+    }
+    if (input < current_optimal) 
     {
         current_optimal = input;
         return true;
     }
     return false;
 }
-void optimal_internal(std::vector<bool> used, std::vector<int> trace, int overall_distance)
+void optimal_internal(std::vector<bool> used, std::vector<int> trace, int overall_distance, std::vector<std::vector<int>>& distance)
 {
     bool is_done = true;
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < distance.size(); i++)
     {
         if (used[i]) continue;
         std::vector<bool> new_used = used;
@@ -105,13 +94,28 @@ void optimal_internal(std::vector<bool> used, std::vector<int> trace, int overal
         std::vector<int> new_trace = trace;
         new_trace.push_back(i);
         is_done = false;
-        optimal_internal(new_used, new_trace, overall_distance + distance[trace[trace.size() - 1]][i]);
+        optimal_internal(new_used, new_trace, overall_distance + distance[trace[trace.size() - 1]][i], distance);
     }
-    if (is_done && is_optimal(overall_distance))
+    if (is_done && is_optimal(overall_distance + distance[trace[trace.size() - 1]][trace[0]]))
     {
-        std::cout << to_string(trace) << " " << overall_distance << "\n";
+        get_current_optimal_trace(trace);
     }
 }
+std::vector<int> get_current_optimal_trace(std::vector<int> trace_input)
+{
+    if (trace_input.size() != 0)
+    {
+        optimal_stack_INTERNAL.push_back(trace_input);
+        return {};
+    }
+    else
+    {
+        if (optimal_stack_INTERNAL.size() == 0) return {};
+        std::vector<int> output = optimal_stack_INTERNAL[0];
+        optimal_stack_INTERNAL.erase(optimal_stack_INTERNAL.begin());
+        return output;
+    }
+} 
 // ...
 
 // Разное
@@ -132,7 +136,7 @@ int randomnumber(int included_min, int included_max)
     dist(rng);
     return dist(rng); 
 }
-void print_step(int step, std::vector<int> trace, int selected_town, int overall_distance)
+void print_step(int step, std::vector<int> trace, int selected_town, int overall_distance, std::vector<std::vector<int>>& distance)
 {
     std::cout << step << ") Trace " << to_string(trace) << " Select " << selected_town 
     << " Distance " << distance[trace[trace.size()-2]][trace[trace.size()-1]] << " Overall Distance " << overall_distance << "\n";
