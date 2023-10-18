be29c1a11bccf7ed83e61599232c57abd31ad6ba
DilsThik <Disblocker.just@gmail.com>
2023-10-18 13:37:21 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,224 @@
+#include "main.h"
+
+// Main code
+int main(int, char**)
+{
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+        return 1;
+
+    // Decide GL+GLSL versions
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+    // GL ES 2.0 + GLSL 100
+    const char* glsl_version = "#version 100";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(__APPLE__)
+    // GL 3.2 + GLSL 150
+    const char* glsl_version = "#version 150";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+#else
+    // GL 3.0 + GLSL 130
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+#endif
+
+    // Create window with graphics context
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", nullptr, nullptr);
+    if (window == nullptr)
+        return 1;
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsLight();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    // Our state
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    // Main loop
+#ifdef __EMSCRIPTEN__
+    // For an Emscripten build we are disabling file-system access, so let's not attempt to do a fopen() of the imgui.ini file.
+    // You may manually call LoadIniSettingsFromMemory() to load settings from your own storage.
+    io.IniFilename = nullptr;
+    EMSCRIPTEN_MAINLOOP_BEGIN
+#else
+    while (!glfwWindowShouldClose(window))
+#endif
+    {
+        glfwPollEvents();
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        const ImGuiViewport* viewport = ImGui::GetMainViewport();
+        ImGui::SetNextWindowPos(viewport->WorkPos);
+        ImGui::SetNextWindowSize(viewport->WorkSize);
+        if (ImGui::Begin("Lab6", NULL, ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings))
+        {
+            static ImVector<ImVec2> points;
+            static ImVec2 scrolling(0.0f, 0.0f);
+            if (ImGui::Button("Back to (0, 0)")) scrolling = ImVec2{0.0f, 0.0f};
+            ImGui::SameLine();
+            ImGui::Text(("scrolling: " + INTSTR(scrolling.x) + "," + INTSTR(scrolling.y)).c_str());
+            static bool opt_enable_grid = true;
+            static bool opt_enable_context_menu = true;
+            static bool adding_point = false;
+
+            // Typically you would use a BeginChild()/EndChild() pair to benefit from a clipping region + own scrolling.
+            // Here we demonstrate that this can be replaced by simple offsetting + custom drawing + PushClipRect/PopClipRect() calls.
+            // To use a child window instead we could use, e.g:
+            //      ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));      // Disable padding
+            //      ImGui::PushStyleColor(ImGuiCol_ChildBg, IM_COL32(50, 50, 50, 255));  // Set a background color
+            //      ImGui::BeginChild("canvas", ImVec2(0.0f, 0.0f), true, ImGuiWindowFlags_NoMove);
+            //      ImGui::PopStyleColor();
+            //      ImGui::PopStyleVar();
+            //      [...]
+            //      ImGui::EndChild();
+
+            // Using InvisibleButton() as a convenience 1) it will advance the layout cursor and 2) allows us to use IsItemHovered()/IsItemActive()
+            ImVec2 canvas_p0 = ImGui::GetCursorScreenPos();      // ImDrawList API uses screen coordinates!
+            ImVec2 canvas_sz = ImGui::GetContentRegionAvail();   // Resize canvas to what's available
+            if (canvas_sz.x < 50.0f) canvas_sz.x = 50.0f;
+            if (canvas_sz.y < 50.0f) canvas_sz.y = 50.0f;
+            ImVec2 canvas_p1 = ImVec2(canvas_p0.x + canvas_sz.x, canvas_p0.y + canvas_sz.y);
+
+            // Draw border and background color
+            ImGuiIO& io = ImGui::GetIO();
+            ImDrawList* draw_list = ImGui::GetWindowDrawList();
+            draw_list->AddRectFilled(canvas_p0, canvas_p1, IM_COL32(50, 50, 50, 255));
+            draw_list->AddRect(canvas_p0, canvas_p1, IM_COL32(255, 255, 255, 255));
+
+
+            const ImVec2 origin(canvas_p0.x + scrolling.x, canvas_p0.y + scrolling.y); // Lock scrolled origin
+            const ImVec2 mouse_pos_in_canvas(io.MousePos.x - origin.x, io.MousePos.y - origin.y);
+            ImGui::SameLine();
+            ImGui::Text(("origin: " + INTSTR(origin.x) + ", " + INTSTR(origin.y) + " adding_point = " + (adding_point ? "true" : "false")).c_str());
+            
+            // This will catch our interactions
+            ImGui::InvisibleButton("canvas", canvas_sz, ImGuiButtonFlags_MouseButtonLeft | ImGuiButtonFlags_MouseButtonRight);
+            const bool is_hovered = ImGui::IsItemHovered(); // Hovered
+            const bool is_active = ImGui::IsItemActive();   // Held
+            // Add first and second point
+            if (is_hovered && !adding_point && ImGui::IsMouseClicked(ImGuiMouseButton_Left))
+            {
+                adding_point = true;
+            }
+            if (adding_point)
+            {
+                if (!ImGui::IsMouseDown(ImGuiMouseButton_Left))
+                {
+                    points.push_back(mouse_pos_in_canvas);
+                    adding_point = false;
+                }
+            }
+
+            // Pan (we use a zero mouse threshold when there's no context menu)
+            // You may decide to make that threshold dynamic based on whether the mouse is hovering something etc.
+            const float mouse_threshold_for_pan = opt_enable_context_menu ? -1.0f : 0.0f;
+            if (is_active && ImGui::IsMouseDragging(ImGuiMouseButton_Right, mouse_threshold_for_pan))
+            {
+                scrolling.x += io.MouseDelta.x;
+                scrolling.y += io.MouseDelta.y;
+            }
+
+            // Context menu (under default mouse threshold)
+            ImVec2 drag_delta = ImGui::GetMouseDragDelta(ImGuiMouseButton_Right);
+            if (opt_enable_context_menu && drag_delta.x == 0.0f && drag_delta.y == 0.0f)
+                ImGui::OpenPopupOnItemClick("context", ImGuiPopupFlags_MouseButtonRight);
+            if (ImGui::BeginPopup("context"))
+            {
+                //if (adding_line)
+                //    points.resize(points.size() - 2);
+                //adding_line = false;
+                if (ImGui::MenuItem("Remove one", NULL, false, points.Size > 0)) { points.resize(points.size() - 2); }
+                if (ImGui::MenuItem("Remove all", NULL, false, points.Size > 0)) { points.clear(); }
+                ImGui::EndPopup();
+            }
+
+            // Draw grid + all lines in the canvas
+            draw_list->PushClipRect(canvas_p0, canvas_p1, true);
+            if (opt_enable_grid)
+            {
+                const float GRID_STEP = 64.0f;
+                for (float x = fmodf(scrolling.x, GRID_STEP); x < canvas_sz.x; x += GRID_STEP)
+                    if (scrolling.x - x == 0.0f) 
+                    {
+                        draw_list->AddLine(ImVec2(canvas_p0.x + x, canvas_p0.y), ImVec2(canvas_p0.x + x, canvas_p1.y), IM_COL32(250, 250, 250, 250));
+                    } 
+                    else
+                    {
+                        draw_list->AddLine(ImVec2(canvas_p0.x + x, canvas_p0.y), ImVec2(canvas_p0.x + x, canvas_p1.y), IM_COL32(200, 200, 200, 40));
+                    }
+                for (float y = fmodf(scrolling.y, GRID_STEP); y < canvas_sz.y; y += GRID_STEP)
+                    if (scrolling.y - y == 0.0f)
+                    {
+                        draw_list->AddLine(ImVec2(canvas_p0.x, canvas_p0.y + y), ImVec2(canvas_p1.x, canvas_p0.y + y), IM_COL32(250, 250, 250, 250));
+                    }
+                    else
+                    {
+                        draw_list->AddLine(ImVec2(canvas_p0.x, canvas_p0.y + y), ImVec2(canvas_p1.x, canvas_p0.y + y), IM_COL32(200, 200, 200, 40));
+                    }
+            }
+            for (int n = 0; n < points.Size; n += 1)
+            {
+                draw_list->AddCircleFilled(ImVec2(ImVec2(origin.x + points[n].x, origin.y + points[n].y)), 6, IM_COL32(255, 255, 0, 255), 4);
+                for (int j = 0; j < n; j++)
+                {
+                    draw_list->AddLine(ImVec2(origin.x + points[n].x, origin.y + points[n].y), ImVec2(origin.x + points[j].x, origin.y + points[j].y), IM_COL32(255, 255, 0, 40), 2.0f);
+                }
+            }
+            for (int n = 0; n < points.Size; n++)
+            {
+                draw_list->AddText(ImVec2(origin.x + points[n].x + 2, origin.y + points[n].y), IM_COL32(255, 255, 255, 255), INTSTR(n + 1).c_str());
+            }
+            draw_list->PopClipRect();
+
+            ImGui::End();
+        }
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+#ifdef __EMSCRIPTEN__
+    EMSCRIPTEN_MAINLOOP_END;
+#endif
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
---
+++ method.cpp
@@ -0,0 +1,141 @@
+#include "main.h"
+
+// Входные данные
+const int N = 5; 
+std::vector<std::vector<int>> distance =
+   {{ 0, 5, 8, 5, 3 },                     
+    { 5, 0, 2, 4, 1 },
+    { 8, 2, 0, 6, 7 },
+    { 5, 4, 6, 0, 9 },
+    { 3, 1, 7, 9, 0 }}; // Оптимальное решение для этой задачи коммивояжёра: 5 -> 1 -> 2 -> 4 -> 3 : расстояние 8
+// ...
+
+// Функции для вычисления точного оптимального значения
+void print_optimal();
+void optimal_internal(std::vector<bool> used, std::vector<int> trace, int overall_distance);
+bool is_optimal(int input);
+// ...
+
+// Разное
+std::string to_string(std::vector<int> input);
+int randomnumber(int included_min, int included_max);
+void print_step(int step, std::vector<int> trace, int selected_town, int overall_distance);
+// ...
+
+int method()
+{
+    std::vector<bool> X = std::vector<bool>(N, false); // Если мы уже входили в город, пишем 1
+    std::vector<int> trace;
+    int overall_distance = 0;
+    int x = randomnumber(0, 4);
+    X[x] = true;
+    trace.push_back(x);
+    std::cout << "1) Trace " << to_string(trace) << " Select " << x << "\n";
+
+    for (int i = 1; i < N; i++) //общий шаг
+    {
+        int current_town = x;
+        std::vector<int> current_min_pairs;
+        for (int j = 0; j < i; j++) //Вычисление минимумов для каждого внутреннего элемента
+        {
+            int current_min_distance = __INT_MAX__;
+            int current_min_town = -1;
+            for (int k = 0; k < N; k++)
+            {
+                if (!X[k] && distance[trace[j]][k] < current_min_distance)
+                {
+                    current_min_distance = distance[trace[j]][k];
+                    current_min_town = k;
+                }
+            }
+            current_min_pairs.push_back(current_min_town);
+        }
+        int current_min_distance_in_pairs = __INT_MAX__;
+        int current_min_to = -1;
+        int current_min_from = -1;
+        for (int j = 0; j < i; j++)
+        {
+            if (distance[trace[j]][current_min_pairs[j]] < current_min_distance_in_pairs)
+            {
+                current_min_from = j;
+                current_min_to = current_min_pairs[j];
+                current_min_distance_in_pairs = distance[trace[j]][current_min_pairs[j]];
+            }
+        }
+        trace.insert(trace.begin() + current_min_from + 1, current_min_to);
+        X[current_min_to] = true;
+        overall_distance += current_min_distance_in_pairs;
+        print_step(i + 1, trace, x, overall_distance);
+    }
+    overall_distance += distance[trace[0]][trace[N-1]];
+
+    std::cout << "Final: Trace " << to_string(trace) << " Distance " << overall_distance << "\n";
+
+    std::cout << "Calculating best possible result:\n";
+    print_optimal();
+}
+
+// Код функций для вычисления точного оптимального значения
+void print_optimal()
+{
+    optimal_internal({ 1, 0, 0, 0, 0}, { 0 }, 0);
+    optimal_internal({ 0, 1, 0, 0, 0}, { 1 }, 0);
+    optimal_internal({ 0, 0, 1, 0, 0}, { 2 }, 0);
+    optimal_internal({ 0, 0, 0, 1, 0}, { 3 }, 0);
+    optimal_internal({ 0, 0, 0, 0, 1}, { 4 }, 0);
+}
+bool is_optimal(int input)
+{
+    static int current_optimal = __INT_MAX__;
+    if (input <= current_optimal) 
+    {
+        current_optimal = input;
+        return true;
+    }
+    return false;
+}
+void optimal_internal(std::vector<bool> used, std::vector<int> trace, int overall_distance)
+{
+    bool is_done = true;
+    for (int i = 0; i < N; i++)
+    {
+        if (used[i]) continue;
+        std::vector<bool> new_used = used;
+        new_used[i].flip();
+        std::vector<int> new_trace = trace;
+        new_trace.push_back(i);
+        is_done = false;
+        optimal_internal(new_used, new_trace, overall_distance + distance[trace[trace.size() - 1]][i]);
+    }
+    if (is_done && is_optimal(overall_distance))
+    {
+        std::cout << to_string(trace) << " " << overall_distance << "\n";
+    }
+}
+// ...
+
+// Разное
+std::string to_string(std::vector<int> input)
+{
+    std::string output = "[ ";
+    for (int i = 0; i < input.size(); i++)
+    {
+        output.append(std::to_string(input[i]) + " ");    
+    }
+    output.append("]");
+    return output;
+}
+int randomnumber(int included_min, int included_max)
+{
+    static std::default_random_engine rng(time(NULL));
+    std::uniform_int_distribution<int> dist(included_min, included_max); 
+    dist(rng);
+    return dist(rng); 
+}
+void print_step(int step, std::vector<int> trace, int selected_town, int overall_distance)
+{
+    std::cout << step << ") Trace " << to_string(trace) << " Select " << selected_town 
+    << " Distance " << distance[trace[trace.size()-2]][trace[trace.size()-1]] << " Overall Distance " << overall_distance << "\n";
+    return;
+}
+// ...
